3ce86d8c3a2b80e03b67fb23b29f7076f7d86e37
cavanjk <cavxn.kestxr@gmail.com>
2025-01-28 10:57:54 -0500

    resource manager

--- src/teams/student/kingVon/resourceManager/ResourceManager.java
+++ src/teams/student/kingVon/resourceManager/ResourceManager.java
@@ -1,5 +1,29 @@
 package teams.student.kingVon.resourceManager;
 
+import objects.entity.unit.Unit;
+import objects.resource.Resource;
+import player.Player;
+import teams.student.kingVon.KingVon;
+
+import java.util.ArrayList;
+
 public class ResourceManager {
 
+    private final Player player;
+    private ArrayList<KingVonResource> resources;
+
+    public ResourceManager(Unit unit) {
+        this.player = unit.getPlayer();
+        resources = new ArrayList<>();
+    }
+
+    public void registerResources() {
+        for (Resource r : player.getAllResources()) {
+            resources.add(new KingVonResource(r.getX(), r.getY()));
+        }
+    }
+
+    public ArrayList<KingVonResource> getResources() {
+        return resources;
+    }
 }
